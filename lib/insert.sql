8ca335b9b01e52c6ba61c3f97dbe796e18b86f3d
Alvin Li <aflip24@gmail.com>
2023-07-28 04:17:14 -0500

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ('Mr. Chocolate', 4, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Rowdy', 6, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Tabitha', 5, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Sergeant Brown', 10, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Melissa', 2, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Grinch', 8, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Wendy', 4, 'F');
+INSERT INTO bears (name, age, sex) VALUES (NULL, 8, 'M');
